267b634d62ae3a8e24c499257cca132530ad2418
Richard Lyman <richard.lyman@gmail.com>
2015-03-07 00:30:03 -0700

    .

--- redisb.go
+++ redisb.go
@@ -8,7 +8,16 @@ import (
 	"strings"
 )
 
+const nilLength = -1
 type RedisNil struct{}
+
+type InternalError struct {
+	e error
+}
+
+func (e InternalError) Error() string {
+	return e.Error()
+}
 
 type RedisError struct {
 	prefix string
@@ -32,76 +41,6 @@ func Do(c net.Conn, args ...string) (interface{}, error) {
 	return decode(bufio.NewReader(c))
 }
 
-func decode(r *bufio.Reader) (interface{}, error) {
-	t, err := r.ReadByte()
-	if err != nil {
-		return nil, err
-	}
-	switch string(t) {
-	case "-":
-		s, err := r.ReadString('\n')
-		if err != nil {
-			return nil, err
-		}
-		return nil, parseError(s)
-	case "+":
-		return r.ReadString('\n')
-	case ":":
-		s, err := r.ReadString('\n')
-		if err != nil {
-			return nil, err
-		}
-		i, err := toInt(s)
-		if err != nil {
-			return nil, err
-		}
-		return i, nil
-	case "$":
-		tmp, err := r.ReadString('\n')
-		if err != nil {
-			return nil, err
-		}
-		slen, err := toInt(tmp)
-		if err != nil {
-			return nil, err
-		}
-		if slen == -1 {
-			return RedisNil{}, nil
-		}
-		s := make([]byte, slen)
-		r.Read(s)
-		r.ReadByte()
-		r.ReadByte()
-		return string(s), nil
-	case "*":
-		tmp, err := r.ReadString('\n')
-		if err != nil {
-			return nil, err
-		}
-		alen, err := toInt(tmp)
-		if err != nil {
-			return nil, err
-		}
-		if alen == -1 {
-			return RedisNil{}, nil
-		}
-		result := make([]interface{}, 0, alen)
-		for i := int64(0); i < alen; i++ {
-			v, err := decode(r)
-			if err != nil {
-				return nil, err
-			}
-			result = append(result, v)
-		}
-		return result, nil
-	}
-	panic(fmt.Sprintf("Failed to identify type: '%q'", string(t)))
-}
-
-func toInt(s string) (int64, error) {
-	return strconv.ParseInt(strings.TrimSpace(s), 10, 64)
-}
-
 func encode(i interface{}) string {
 	switch t := i.(type) {
 	case []string:
@@ -115,4 +54,86 @@ func encode(i interface{}) string {
 	default:
 		panic(fmt.Sprintf("Unable to encode type: %#v", t))
 	}
+}
+
+func decode(r *bufio.Reader) (interface{}, error) {
+	t, err := r.ReadByte()
+	if err != nil {
+		return nil, InternalError{err}
+	}
+	switch string(t) {
+	case "-":
+		s, err := r.ReadString('\n')
+		if err != nil {
+			return nil, InternalError{err}
+		}
+		return nil, parseError(s)
+	case "+":
+		return r.ReadString('\n')
+	case ":":
+		return decodeIntSuffix(r)
+	case "$":
+		return decodeBulkStringSuffix(r)
+	case "*":
+		return decodeArraySuffix(r)
+	}
+	panic(fmt.Sprintf("Failed to identify type: '%q'", string(t)))
+}
+
+func decodeIntSuffix(r *bufio.Reader) (interface{}, error) {
+	s, err := r.ReadString('\n')
+	if err != nil {
+		return nil, InternalError{err}
+	}
+	i, err := toInt(s)
+	if err != nil {
+		return nil, InternalError{err}
+	}
+	return i, nil
+}
+
+func decodeBulkStringSuffix(r *bufio.Reader) (interface{}, error) {
+	tmp, err := r.ReadString('\n')
+	if err != nil {
+		return nil, InternalError{err}
+	}
+	slen, err := toInt(tmp)
+	if err != nil {
+		return nil, InternalError{err}
+	}
+	if slen == nilLength {
+		return RedisNil{}, nil
+	}
+	s := make([]byte, slen)
+	r.Read(s)
+	r.ReadByte()
+	r.ReadByte()
+	return string(s), nil
+}
+
+func decodeArraySuffix(r *bufio.Reader) (interface{}, error) {
+	tmp, err := r.ReadString('\n')
+	if err != nil {
+		return nil, InternalError{err}
+	}
+	alen, err := toInt(tmp)
+	if err != nil {
+		return nil, InternalError{err}
+	}
+	if alen == nilLength {
+		return RedisNil{}, nil
+	}
+	result := make([]interface{}, 0, alen)
+	for i := int64(0); i < alen; i++ {
+		v, err := decode(r)
+		if err != nil {
+			return nil, InternalError{err}
+		}
+		result = append(result, v)
+	}
+	return result, nil
+}
+
+func toInt(s string) (int64, error) {
+	return strconv.ParseInt(strings.TrimSpace(s), 10, 64)
 }
